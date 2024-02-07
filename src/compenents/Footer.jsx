43daa7ff13cc7030d0c7df0bf16a015249410ce6
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2024-02-07 13:45:16 +0100

    Footer added

---
+++ src/compenents/Footer.jsx
@@ -0,0 +1,47 @@
+import { useTranslation } from "react-i18next";
+
+function Footer() {
+  const { t } = useTranslation();
+
+  return (
+    <footer className="bg-dark text-white text-center py-4">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <ul className="list-inline">
+              <li className="list-inline-item">
+                <a
+                  href="https://github.com/Kasia307584"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="bi bi-github"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a
+                  href="https://www.linkedin.com/in/katarzyna-zurek-423b0664/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="bi bi-linkedin"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <p>
+              &copy; {new Date().getFullYear()}
+              &nbsp;
+              {t("Katarzyna Zurek. All rights reserved.")}
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
