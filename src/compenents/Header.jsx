1870d231bef8ab455459f26ac150b673e4cbceb2
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2024-02-14 17:30:22 +0100

    Header section styled

--- src/compenents/Header.jsx
+++ src/compenents/Header.jsx
@@ -5,28 +5,32 @@ function Header() {
   const { t } = useTranslation();
 
   return (
-    <section id="section-header" className="section section-header">
-      <div className=" mb-3">
-        <div className="row g-0">
-          <div className="col-md-8">
-            <div className="card-body">
-              <h2 className="card-title">{t("Hello, I'm Katarzyna")}</h2>
-              <h1 className="text-uppercase"> {t("presentation.title")}</h1>
-              <HashLink smooth to="#section-projects" className="nav-link ">
-                {t("Projects")}
-              </HashLink>
-            </div>
+    <section
+      id="section-header"
+      className="section section-header mb-3 text-center "
+    >
+      <div className="row align-items-center justify-content-around">
+        <div className="col-md-8">
+          <div className="card-body">
+            <h2 className="card-title">{t("Welcome, I'm Katarzyna")}</h2>
+            <h1 className="text-uppercase"> {t("presentation.title")}</h1>
+            <HashLink to="#section-projects" className="nav-link ">
+              <button type="button" className="btn btn-outline-warning">
+                <i className="bi bi-arrow-down"></i> &nbsp;
+                {t("My projects")}
+              </button>
+            </HashLink>
           </div>
-          <div className="col-md-4">
-            <img
-              src="/logos/reactjs.svg"
-              x="0"
-              y="100"
-              width="200"
-              height="200"
-              alt="Computer image"
-            />
-          </div>
+        </div>
+        <div className="col-md-4 ">
+          <img
+            src="/logos/reactjs.svg"
+            x="0"
+            y="100"
+            width="200"
+            height="200"
+            alt="Computer image"
+          />
         </div>
       </div>
     </section>
