3e8bb6ec29772fae812c3f5b880ad034f1c560b8
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2024-02-15 12:25:50 +0100

    Skills and Header sections adjustments

--- src/compenents/SectionSkills.jsx
+++ src/compenents/SectionSkills.jsx
@@ -2,23 +2,71 @@ function SectionSkills(props) {
   const { title } = props;
   return (
     <section id="section-skills" className="section section-skills  mb-3 ">
-      <div className="card-body">
-        <h1 className="card-title">{title}</h1>
-        <div style={{ margin: "auto" }}>
-          <img src="/logos/html5.svg" x="0" y="100" width="90" height="90" />
-          <img src="/logos/css3.svg" x="0" y="100" width="90" height="90" />
-          <img
-            src="/logos/javascript.svg"
-            x="0"
-            y="100"
-            width="90"
-            height="90"
-          />
-          <img src="/logos/reactjs.svg" x="0" y="100" width="90" height="90" />
-          <img src="/logos/sass.svg" x="0" y="100" width="90" height="90" />
-          <img src="/logos/git.svg" x="0" y="100" width="90" height="90" />
-          <img src="/logos/github.svg" x="0" y="100" width="90" height="90" />
-        </div>
+      <h1>{title}</h1>
+      <div>
+        <img
+          src="/logos/html5.svg"
+          x="0"
+          y="100"
+          width="90"
+          height="90"
+          alt="Html5 logo"
+          title="Html5"
+        />
+        <img
+          src="/logos/css3.svg"
+          x="0"
+          y="100"
+          width="90"
+          height="90"
+          alt="Css3 logo"
+          title="Css3"
+        />
+        <img
+          src="/logos/javascript.svg"
+          x="0"
+          y="100"
+          width="90"
+          height="90"
+          alt="Javascript logo"
+          title="JavaScript"
+        />
+        <img
+          src="/logos/reactjs.svg"
+          x="0"
+          y="100"
+          width="90"
+          height="90"
+          alt="React logo"
+          title="React"
+        />
+        <img
+          src="/logos/sass.svg"
+          x="0"
+          y="100"
+          width="90"
+          height="90"
+          alt="Sass logo"
+          title="Sass"
+        />
+        <img
+          src="/logos/git.svg"
+          x="0"
+          y="100"
+          width="90"
+          height="90"
+          alt="Git logo"
+          title="Git"
+        />
+        <img
+          src="/logos/github.svg"
+          x="0"
+          y="100"
+          width="90"
+          height="90"
+          alt="Github logo"
+          title="GitHub"
+        />
       </div>
     </section>
   );
